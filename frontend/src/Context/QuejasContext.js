fbea6afd0e725ceaffac326c715123add07619d2
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-08-03 19:47:56 -0600

    fixed grammar/ortographic horror

--- frontend/src/Context/QuejasContext.js
+++ frontend/src/Context/QuejasContext.js
@@ -6,7 +6,7 @@ const initialQuejas = null
 
 export const QuejasContextProvider = ({children}) =>{
 
-    // NO HE ACABADO DE ESCRIBIR ESTE CONTEXTO PERO LA INTENSION ES VER SI PUEDO HACER QUE QUEJAS Y  LA LOGICA DE QUEJASBYCATEGORY/QUEJASBYSECTOR SEA UN ESTADO GLOBAL VIA CONTEXTO?? Para el menu nav quiza aplica un state condicional segun donde clickas... pero para la home que renderiza ambas no se si eso jale... // al final aca lo importante es poder correr el hook useQuejasByCategory en combo con useFetch sin que haya competencia que genere null como esta pasndo ahora... y evitar tambien tener duplicaciones de mismo codigo all over de place
+    // NO HE ACABADO DE ESCRIBIR ESTE CONTEXTO PERO LA INTENCION ES VER SI PUEDO HACER QUE QUEJAS Y  LA LOGICA DE QUEJASBYCATEGORY/QUEJASBYSECTOR SEA UN ESTADO GLOBAL VIA CONTEXTO?? Para el menu nav quiza aplica un state condicional segun donde clickas... pero para la home que renderiza ambas no se si eso jale... // al final aca lo importante es poder correr el hook useQuejasByCategory en combo con useFetch sin que haya competencia que genere null como esta pasndo ahora... y evitar tambien tener duplicaciones de mismo codigo all over de place
 
     //ojo aunque ya cree el context provider me falta hacer el llamado de USECONTEXT() Donde sea que lo planee/decida usar... veremos luego si se logra usar como pienso q debeser ... mientras tanto... la app ya funciona sin el contexto
 
