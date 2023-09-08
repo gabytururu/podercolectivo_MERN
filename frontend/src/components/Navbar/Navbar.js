12c0c841d99bc89cf1bafad7861c6f2c11f11fe3
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-09-08 13:08:23 -0600

    MILESTONE_ responsive NavBar implemented & completed

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,26 @@
 import {Link, NavLink} from 'react-router-dom'
 import './Navbar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faBars, faX} from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
 import {useState} from 'react'
 
 const Navbar = () => {
 
     const [openNavBar, setOpenNavbar] = useState(true)
+    
+    // substituted by function ((curr) => !curr en el onclick)
+    // const checkOpenNavBar = (openNavBar) =>{
+    //     if(openNavBar === false){
+    //         setOpenNavbar(true)
+    //     }else{
+    //         setOpenNavbar(false)
+    //     }
+    // }
 
+    
+  
     return ( 
        
             <header className="navbar">
@@ -18,21 +31,35 @@ const Navbar = () => {
                         </Link>
                     </div>
                     <div className="menu">
-                        <NavLink exact activeClassName= "active" o="/">Inicio</NavLink>
+                        <NavLink exact activeClassName= "active" to="/">Inicio</NavLink>
                         <NavLink activeClassName= "active" to="/sectores">Quejas por Sector</NavLink>
                         <NavLink activeClassName= "active" to="/empresas">Quejas por Empresa</NavLink>
                         <NavLink activeClassName= "active" to="/poner-queja">Poner una Queja</NavLink>
                     </div>
 
-                    {/* NOT WORKING BC NOW I NEED TO SET THE OPOSITE FOR CLOSE NAVBAR OR SOMETHING SIMILAR TO TOGGLE --- CHECK HOW TO DO TAHT ALSO FINISH PEDROTECH TUTORIAL BC I NEED TO ALSO RENDER THE MENU AS IT SHOWS UP*/}
-                    {/* ALSO... MUST CHECK AGAIN WHAT TO DO WITH FONTAWESOME BC IT IS NOW IMPORTIN ALL LIBRARY OF SVGS SO IT TRIES TO UPLOAD ON GITHUB :( SIMILAR TO THE NODEMODULES THING ya entendi porque... ES PORQUE INSTALE TODO EN PODER COLECTIVO Y DEBIO SER EN FRONTEND ---!! DESINSTALAR TODO, VA PA TRAS Y VOLVER A ESTABLECER LOS ICONOS DESDE CD/FRONTEND Y BORRAR LAS CARPETAS DE NODEMODULES Y PACKAGE JSON NUEVOS*/}
-                    <div className="mobile" onClick={()=>{setOpenNavbar(false)}}>
-                    { openNavBar ? 
-                        <FontAwesomeIcon icon={faBars} className="icon open" />
-                        :
-                        <FontAwesomeIcon icon={faX} className="icon close"/>
-                    }
+                    {/* <div className="mobile" onClick={()=>{checkOpenNavBar(openNavBar)}}> */}
+                    <div className="mobile" onClick={()=>{setOpenNavbar(curr=>!curr)}}>
+                        { openNavBar ?                             
+                                <FontAwesomeIcon icon={faBars} className="icon" />
+                                :
+                                <FontAwesomeIcon icon={faX} className="icon"/>
+                        }
                     </div>
+
+                    <div className="overlayBox" >
+                        {openNavBar ?!
+                            ''
+                        :                            
+                            <div className="menuOverlay">
+                                <NavLink exact activeClassName= "active" to="/" onClick={()=>{setOpenNavbar(curr=>!curr)}}>Inicio</NavLink>
+                                <NavLink activeClassName= "active" to="/sectores" onClick={()=>{setOpenNavbar(curr=>!curr)}}>Quejas por Sector</NavLink>
+                                <NavLink activeClassName= "active" to="/empresas" onClick={()=>{setOpenNavbar(curr=>!curr)}}>Quejas por Empresa</NavLink>
+                                <NavLink activeClassName= "active" to="/poner-queja" onClick={()=>{setOpenNavbar(curr=>!curr)}}>Poner una Queja</NavLink>
+                            </div>       
+                        }
+                    </div>
+
+                   
 
                 </nav>
             </header>
