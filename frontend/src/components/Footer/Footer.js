873b0c2302bafb2f6f0d88a78d1bc0e6764be149
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-12-07 21:26:33 -0600

    WIP_Starting to improve footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return ( 
         <footer className="footerWrapper">
                 <div className="footLogo">
-                    <img src="./poderColectivoLOGO_BL_CLEAR.png" alt="logotipo poder colectivo" />
+                    {/* <img src="/poderColectivoLOGO_BL_CLEAR.png" alt="logotipo poder colectivo" /> */}
                 </div>
                 <div className="footLegend">
                     <p>Un sitio hecho con <span>H</span> de Mexas para Mexas</p>
